a76eae477ad492874b1f68ef20624345dda1dd1d
A'zamjon Abdumuxtorov <azamjonabdumuxtorov1220@gmail.com>
2025-12-12 18:21:01 +0500

    Initial Commit

---
+++ ToDo_Click/ToDo_ClickApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDo_ClickApp.swift
+//  ToDo_Click
+//
+//  Created by A'zamjon Abdumuxtorov on 12/12/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDo_ClickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
